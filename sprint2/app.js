f9a54e0e5cca7fbb45db380cddc1b9d35495fffb
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-10 09:16:30 -0300

    estructura inicial

---
+++ sprint2/app.js
@@ -0,0 +1,13 @@
+
+const express = require ('express');
+const app = express();
+const path = require ('path');
+const port = 3030;
+
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )))
+
+
+
+
+app.listen(port, () => console.log('Servidor corriendo en el puerto' + port));
