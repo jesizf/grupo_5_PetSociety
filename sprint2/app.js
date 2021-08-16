cba46d1b4d2084c9c32f92cddbb31769e5fb8c9c
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-16 15:49:35 -0300

    footer vista seccion dudas en proceso

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,6 +6,9 @@ const port = 3030;
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )))
+app.get('/como-comprar', (req, res) => res.sendFile(path.join(__dirname, 'views', 'como-comprar.html' )));
+app.get('/cambios-y-devoluciones', (req, res) => res.sendFile(path.join(__dirname, 'views', 'cambios-devoluciones.html' )));
+app.get('/formas-de-pago', (req, res) => res.sendFile(path.join(__dirname, 'views', 'formas-pagos.html' )))
 
 
 
