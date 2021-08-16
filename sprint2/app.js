3b8774b45edce21b82692e4942a99c8029b91edb
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-16 12:17:31 -0300

    agregando vistas del footer

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,8 +5,9 @@ const path = require ('path');
 const port = 3030;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )))
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )));
+app.get('/terminos-y-condiciones', (req, res) => res.sendFile(path.join(__dirname, 'views', 'terminos.html'))) ;
+app.get('/nosotros', (req, res) => res.sendFile(path.join(__dirname, 'views', 'nosotros.html'))) ;
 
 
 
