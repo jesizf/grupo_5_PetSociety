78b5b9abada94cb61e29d9553f64e75533215db8
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-08-20 16:38:21 -0300

    home, login y register fuencionando

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,9 +5,6 @@ const path = require ('path');
 const port = 3030;
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )));
-app.get('/terminos-y-condiciones', (req, res) => res.sendFile(path.join(__dirname, 'views', 'terminos.html'))) ;
-app.get('/nosotros', (req, res) => res.sendFile(path.join(__dirname, 'views', 'nosotros.html'))) ;
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html' )))
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
