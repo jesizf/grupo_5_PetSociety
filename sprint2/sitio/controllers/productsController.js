ee673c54d5134ea1969826f487186d3fe2cd9e91
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-10 13:55:23 -0300

    creando vista agregar productos

--- sprint2/sitio/controllers/productsController.js
+++ sprint2/sitio/controllers/productsController.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-
+let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
 
 module.exports = {
-     
+    add : (req,res) => {
+        return res.render('productAdd',{ title: 'Agregar Productos',
+            products
+        })
+    },
 }
---
+++ sprint2/sitio/routes/products.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET products page. */
+const {add}=require('../controllers/productsController');
+router.get('/add', add)
+
+module.exports = router;
