cef58cb402d805dc3f4d804fb70ad9306ada7867
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-10 11:51:03 -0300

    forech products perros

--- sprint2/sitio/controllers/mainController.js
+++ sprint2/sitio/controllers/mainController.js
@@ -1,10 +1,11 @@
  const fs = require('fs');
  const path = require('path');
+ let products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'), 'utf-8'));
  
  
  module.exports = {
     index: (req,res)=>{
-        return res.render('home', {title: 'Pet Society'})
+        return res.render('home', {title: 'Pet Society',  products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8')),})
     },
     cart: (req,res)=> {
         return res.render('carrito',{title: 'carrito'})
