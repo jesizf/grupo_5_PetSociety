8a051a22631333cea1c8d2ea3921289699f6aa01
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-13 19:54:05 -0300

    filtar vista de categorias del home

--- sprint2/sitio/controllers/mainController.js
+++ sprint2/sitio/controllers/mainController.js
@@ -1,11 +1,22 @@
  const fs = require('fs');
  const path = require('path');
- let products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'), 'utf-8'));
+
+ let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
+ 
+ 
+
  
  
  module.exports = {
     index: (req,res)=>{
-        return res.render('home', {title: 'Pet Society',  products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8')),})
+        let productsPerros = products.filter(products => products.category === 'perro');
+        let productsGatos = products.filter(products => products.category === 'gato');
+        return res.render('home', {title: 'Pet Society', 
+       products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8')),
+       productsPerros,
+       productsGatos
+    })
+    
     },
     cart: (req,res)=> {
         return res.render('carrito',{title: 'carrito'})
--- sprint2/sitio/routes/products.js
+++ sprint2/sitio/routes/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET products page. */
-const {add}=require('../controllers/productsController');
+const {add} = require('../controllers/productsController');
 router.get('/add', add)
 
 module.exports = router;
