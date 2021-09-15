3c5af4bfb1b91c9d22f07a4881dba0c42792180a
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-15 15:21:58 -0300

    vista add products

--- sprint2/sitio/controllers/productsController.js
+++ sprint2/sitio/controllers/productsController.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
-
+let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
+let  categories = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','categories.json'),'utf-8'));
+const firstLetter = require('../utils/firstLetter');
 module.exports = {
     add : (req,res) => {
         return res.render('productAdd',{ title: 'Agregar Productos',
-            products
+            products,
+            categories,
+            firstLetter
         })
     },
 }
