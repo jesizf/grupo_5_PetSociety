32abd26bdc4ac68e3740a0d0bb96233f80aecde2
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-16 09:33:30 -0300

    editando formulario add

--- sprint2/sitio/controllers/productsController.js
+++ sprint2/sitio/controllers/productsController.js
@@ -3,12 +3,14 @@ const path = require('path');
 let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
 let  categories = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','categories.json'),'utf-8'));
 const firstLetter = require('../utils/firstLetter');
+const pesoProducts = require('../data/pesoProducts.json')
 module.exports = {
     add : (req,res) => {
         return res.render('productAdd',{ title: 'Agregar Productos',
             products,
             categories,
-            firstLetter
+            firstLetter,
+            pesoProducts
         })
     },
     detail : (req, res) =>{
