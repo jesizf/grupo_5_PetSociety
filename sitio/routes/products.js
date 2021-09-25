270a0c608ceeac957b82a4a8213b57bf7c93cb83
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-09-25 15:55:46 -0300

    mover las img a la carpeta products

--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET products page. */
 const {detail, add,edit, search, store} = require('../controllers/productsController');
 router.get('/detail/:id', detail)
