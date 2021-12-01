7a9d7181305a0d8017e9f524796e687a717abd85
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-12-01 14:08:46 -0300

    solucionando problema de img

---
+++ sitio/prueba.js
@@ -0,0 +1,240 @@
+/*'use strict';
+
+let products = [
+  {
+      "file": [
+          "img-producto1633610759495.jpg",
+          "img-producto1633610759498.jpg",
+          "img-producto1633610759501.jpg",
+          "img-producto1633610759504.jpg"
+       ],
+       "productId": 1,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+   
+  },
+  {
+     "file": [
+        "img-producto1633611293174.png",
+        "img-producto1633611293175.png",
+        "img-producto1633611293178.png",
+        "img-producto1633611293179.png"
+     ],
+     "productId": 2,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633611435406.png",
+        "img-producto1633611435408.png",
+        "img-producto1633611435409.png",
+        "img-producto1633611435410.png"
+     ],
+     "productId": 3,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633611825905.webp",
+        "img-producto1633611825906.jpg",
+        "img-producto1633611825909.jpg",
+        "img-producto1633611825910.jpg"
+     ],
+     "productId": 4,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633892695039.jpg",
+        "img-producto1633892695075.jpg",
+        "img-producto1633892695080.jpg",
+        "img-producto1633892695083.jpg"
+     ],
+     "productId": 5,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633892826163.jpg",
+        "img-producto1633892826166.jpg",
+        "img-producto1633892826167.jpg",
+        "img-producto1633892826990.jpg"
+     ],
+     "productId": 6,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633893299069.webp",
+        "img-producto1633893299071.webp",
+        "img-producto1633893299072.webp",
+        "img-producto1633893299073.webp"
+     ],
+     "productId": 7,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633893800580.webp",
+        "img-producto1633893800581.webp",
+        "img-producto1633893800582.webp",
+        "img-producto1633893800579.webp"
+     ],
+     "productId": 8,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633894139746.webp",
+        "img-producto1633894139743.webp",
+        "img-producto1633894139744.webp",
+        "img-producto1633894139747.webp"
+     ],
+     "productId": 9,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633894971354.jpg",
+        "img-producto1633894971345.jpg",
+        "img-producto1633894971347.jpg",
+        "img-producto1633894971353.jpg"
+     ],
+     "productId": 10,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633895135178.jpg",
+        "img-producto1633895135443.jpg",
+        "img-producto1633895135447.jpg",
+        "img-producto1633895135450.jpg"
+     ],
+     "productId": 11,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+         
+        "img-producto1633895451241.webp",
+        "img-producto1633895451243.webp",
+        "img-producto1633895451243.webp",
+        "img-producto1633895451245.webp"
+     ],
+     "productId": 12,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+         
+        "img-producto1633895625056.webp",
+        "img-producto1633895625058.webp",
+        "img-producto1633895625060.webp",
+        "img-producto1633895625057.webp"
+     ],
+     "productId": 13,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+         
+        "img-producto1633895784864.webp",
+        "img-producto1633895784865.webp",
+        "img-producto1633895784866.webp",
+        "img-producto1633895784867.webp"
+     ],
+     "productId": 14,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+        "img-producto1633895905755.webp",
+        "img-producto1633895905756.webp",
+        "img-producto1633895905760.webp",
+        "img-producto1633895905761.webp"
+     ],
+     "productId": 15,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  },
+  {
+     "file": [
+         
+        "img-producto1633896160460.webp",
+        "img-producto1633896160461.webp",
+        "img-producto1633896160463.jpg",
+        "img-producto1633896160464.jpg"
+     ],
+     "productId": 16,
+       "createdAt": new Date,
+       "updatedAt": new Date,
+  }
+]
+
+  
+let img = products.map(product =>{
+    let image = {
+      productId : product.productId,
+      file: product.file,
+      createdAt : new Date,
+      updatedAt : new Date,
+    }
+    return image
+  })
+   */
+  
+    
+ 
+      
+        
+  
+
+       
+  let imagenes = [
+        "img-producto1633610759495.jpg", "img-producto1633610759498.jpg","img-producto1633610759501.jpg","img-producto1633610759504.jpg","img-producto1633611293174.png","img-producto1633611293175.png","img-producto1633611293178.png","img-producto1633611293179.png","img-producto1633611435406.png","img-producto1633611435408.png","img-producto1633611435409.png","img-producto1633611435410.png",     "img-producto1633611825905.webp","img-producto1633611825906.jpg","img-producto1633611825909.jpg","img-producto1633611825910.jpg","img-producto1633892695039.jpg","img-producto1633892695075.jpg","img-producto1633892695080.jpg","img-producto1633892695083.jpg",
+          "img-producto1633892826163.jpg","img-producto1633892826166.jpg","img-producto1633892826167.jpg","img-producto1633892826990.jpg","img-producto1633893299069.webp","img-producto1633893299071.webp","img-producto1633893299072.webp","img-producto1633893299073.webp",
+          "img-producto1633893800580.webp","img-producto1633893800581.webp", "img-producto1633893800582.webp","img-producto1633893800579.webp",
+          "img-producto1633894139746.webp","img-producto1633894139743.webp","img-producto1633894139744.webp","img-producto1633894139747.webp","img-producto1633894971354.jpg","img-producto1633894971345.jpg","img-producto1633894971347.jpg","img-producto1633894971353.jpg","img-producto1633895135178.jpg","img-producto1633895135443.jpg","img-producto1633895135447.jpg","img-producto1633895135450.jpg","img-producto1633895451241.webp","img-producto1633895451243.webp","img-producto1633895451243.webp","img-producto1633895451245.webp", "img-producto1633895625056.webp","img-producto1633895625058.webp","img-producto1633895625060.webp","img-producto1633895625057.webp","img-producto1633895784864.webp","img-producto1633895784865.webp","img-producto1633895784866.webp","img-producto1633895784867.webp", "img-producto1633895905755.webp","img-producto1633895905756.webp","img-producto1633895905760.webp","img-producto1633895905761.webp", "img-producto1633896160460.webp","img-producto1633896160461.webp","img-producto1633896160463.jpg","img-producto1633896160464.jpg"
+        ]
+
+          let img = 0;
+          const images = [];
+          
+          
+          for (let i = 0; i < 16; i++) {
+            for (let index = 0; index < 4; index++) {
+              var image = {
+                file : imagenes[img] ,
+                productId : i + 1,
+                createdAt : new Date,
+                updatedAt : new Date
+              }
+              images.push(image)
+    
+              ++img
+
+            
+          } 
+          
+          
+          }
+          console.log(images);
+          
+
+
+
+
+          img-producto1633611435410.png
